c4d27af9d9d4682446f10b7e463a907089bc4514
Nina Popovici <adrenocrom_pn@yahoo.com>
2024-07-15 13:03:41 +0100

    Dashboard frontend

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -1,105 +1,92 @@
 import React from 'react';
 import {
-    MdDashboard,
-    MdOutlineAddTask,
-    MdOutlinePendingActions,
-    MdSettings,
-    MdTaskAlt,
-  } from "react-icons/md";
+  MdDashboard,
+  MdOutlineAddTask,
+  MdSettings,
+  MdTaskAlt,
+} from "react-icons/md";
 import { FaTasks, FaTrashAlt, FaUsers } from "react-icons/fa";
-// import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
-// import { setOpenSidebar } from "../redux/slices/authSlice";
 import clsx from "clsx";
-import AuthService from '../../services/AuthService';
-
 
 const linkData = [
-    {
-      label: "Dashboard",
-      link: "/Dashboard",
-      icon: <MdDashboard />,
-    },
-    {
-      label: "Walls",
-      link: "/WallsDashboard",
-      icon: <FaTasks />,
-    },
-    {
-      label: "Artworks",
-      link: "/ArtworksDashboard",
-      icon: <MdTaskAlt />,
-    },
-    {
-      label: "Users",
-      link: "/Users",
-      icon: <FaUsers />,
-    },
-    {
-      label: "Trash",
-      link: "/Trash",
-      icon: <FaTrashAlt />,
-    },
-  ];
+  {
+    label: "Dashboard",
+    link: "/Dashboard",
+    icon: <MdDashboard />,
+  },
+  {
+    label: "Walls",
+    link: "/WallsDashboard",
+    icon: <FaTasks />,
+  },
+  {
+    label: "Artworks",
+    link: "/ArtworksDashboard",
+    icon: <MdTaskAlt />,
+  },
+  {
+    label: "Users",
+    link: "/Users",
+    icon: <FaUsers />,
+  },
+  {
+    label: "Trash",
+    link: "/Trash",
+    icon: <FaTrashAlt />,
+  },
+];
 
 const Sidebar = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+  const location = useLocation();
+  const path = location.pathname;
 
-// const {user} = useSelector((state) => state.auth);
+  const sidebarLinks = !user?.isAdmin ? linkData : linkData.slice(0, 5);
 
-// const dispatch = useDispatch();
+  // Close the sidebar on a mobile (if you have a function to do this, otherwise remove this function)
+  const closeSidebar = () => {
+    // dispatch(setOpenSidebar(false));
+  };
 
-const user = localStorage.getItem('user');
-const location = useLocation();
-
-const path = location.pathname.split("/")[1];
-const sidebarLinks = !user?.isAdmin ? linkData : linkData.slice(0, 5);
-
-//close the sidebar on a mobile
-const closeSidebar = () => {
-    dispatch(setOpenSidebar(false));
-};
-const NavLink = ({ el }) => {
+  const NavLink = ({ el }) => {
+    const isActive = path === el.link;
     return (
       <Link
-      to={el.link}
-      onClick={closeSidebar}
-      className={clsx(
-        "w-full lg:w-3/4 flex gap-2 px-3 py-2 rounded-full items-center text-white text-base hover:bg-[#9c3abfa7]",
-        path === el.link.split("/")[0] ? "bg-purple-700 text-neutral-100" : ""
-      )}
-    >
-      {el.icon}
-      <span className='hover:text-[#b444d0]'>{el.label}</span>
-    </Link>
+        to={el.link}
+        onClick={closeSidebar}
+        className={clsx(
+          "w-full lg:w-3/4 flex gap-2 px-3 py-2 rounded-full items-center text-base hover:bg-[#9c3abfa7]",
+          isActive ? "bg-purple-700 text-neutral-100" : "text-white"
+        )}
+      >
+        {el.icon}
+        <span className={isActive ? 'text-neutral-100' : 'hover:text-[#b444d0]'}>{el.label}</span>
+      </Link>
     );
   };
 
-
   return (
     <div className='w-full h-full flex flex-col gap-6 p-5'>
-<h1 className='flex gap-1 items-center font-raleway'>
-<p className='bg-purple-400 p-2 rounded-full'>
-    <MdOutlineAddTask className='text-white text-2xl font-black' />
-</p>
-<span className='text-2xl font-bold text-white font-raleway'>Admin Panel</span>
-</h1>
-<div className='flex-1 flex flex-col gap-y-5 py-8 font-raleway'>
-{
-    sidebarLinks.map((link) =>(
-      <NavLink el={link} key= {link.label} />
-    ))
-}
-</div>
-
-<div className=''>
-        <button className='w-full flex gap-2 p-2 items-center text-lg text-white '>
+      <h1 className='flex gap-1 items-center font-raleway'>
+        <p className='bg-purple-400 p-2 rounded-full'>
+          <MdOutlineAddTask className='text-white text-2xl font-black' />
+        </p>
+        <span className='text-2xl font-bold text-white font-raleway'>Admin Panel</span>
+      </h1>
+      <div className='flex-1 flex flex-col gap-y-5 py-8 font-raleway'>
+        {sidebarLinks.map((link) => (
+          <NavLink el={link} key={link.label} />
+        ))}
+      </div>
+      <div>
+        <button className='w-full flex gap-2 p-2 items-center text-lg text-white'>
           <MdSettings />
           <span>Settings</span>
         </button>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- src/pages/WallsDashboard.jsx
+++ src/pages/WallsDashboard.jsx
@@ -1,7 +1,6 @@
-import { Sidebar, MobileSidebar } from '../components/dashboard';
-import React from "react";
+import React, { useState } from "react";
 import {
-  MdAdminPanelSettings,
+  MdMenu,
   MdKeyboardArrowDown,
   MdKeyboardArrowUp,
   MdKeyboardDoubleArrowUp,
@@ -15,94 +14,90 @@ import { summary } from "../assets/data";
 import clsx from "clsx";
 import { BGS, PRIOTITYSTYLES, TASK_TYPE, getInitials } from "../utils/index.js";
 import UserInfo from "../components/dashboard/UserInfo";
-import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
-import { Toaster } from "sonner";
+import { Outlet } from "react-router-dom";
 import styles from '../style';
 import Footer from '../components/Footer.jsx';
 import BackToTopButton from '../components/BackToTopButton.jsx';
+import Sidebar from '../components/dashboard/Sidebar';
+import MobileSidebar from '../components/dashboard/MobileSidebar';
 
 const TaskTable = ({ tasks }) => {
-    const ICONS = {
-      high: <MdKeyboardDoubleArrowUp />,
-      medium: <MdKeyboardArrowUp />,
-      low: <MdKeyboardArrowDown />,
-    };
-  
-    const TableHeader = () => (
-      <thead className='border-b border-gray-300 '>
-        <tr className='text-black text-left'>
-          <th className='py-2'>Wall Title</th>
-          <th className='py-2'>Status</th>
-          <th className='py-2'>Description</th>
-          <th className='py-2'>Location</th>
-          <th className='py-2'>User</th>
-          <th className='py-2 hidden md:block'>Created At</th>
-        </tr>
-      </thead>
-    );
-  
-    const TableRow = ({ task }) => (
-        <tr className='border-b border-gray-300 text-gray-600 hover:bg-gray-300/10'>
-          <td className='py-2'>
-            <div className='flex items-center gap-2'>
-              <div
-                className={clsx("w-4 h-4 rounded-full", TASK_TYPE[task.stage])}
-              />
-    
-              <p className='text-base text-black'>{task.title}</p>
+  const ICONS = {
+    high: <MdKeyboardDoubleArrowUp />,
+    medium: <MdKeyboardArrowUp />,
+    low: <MdKeyboardArrowDown />,
+  };
+
+  const TableHeader = () => (
+    <thead className='border-b border-gray-300 '>
+      <tr className='text-black text-left'>
+        <th className='py-2'>Wall Title</th>
+        <th className='py-2'>Status</th>
+        <th className='py-2'>User</th>
+        <th className='py-2'>Location</th>
+        <th className='py-2'>Description</th>
+        <th className='py-2 hidden md:block'>Created At</th>
+      </tr>
+    </thead>
+  );
+
+  const TableRow = ({ task }) => (
+    <tr className='border-b border-gray-300 text-gray-600 hover:bg-gray-300/10'>
+      <td className='py-2'>
+        <div className='flex items-center gap-2'>
+          <div
+            className={clsx("w-4 h-4 rounded-full", TASK_TYPE[task.stage])}
+          />
+          <p className='text-base text-black'>{task.title}</p>
+        </div>
+      </td>
+      <td className='py-2'>
+        <div className='flex gap-1 items-center'>
+          <span className={clsx("text-lg", PRIOTITYSTYLES[task.priority])}>
+            {ICONS[task.priority]}
+          </span>
+          <span className='capitalize'>{task.priority}</span>
+        </div>
+      </td>
+      <td className='py-2'>
+        <div className='flex'>
+          {task.team.map((m, index) => (
+            <div
+              key={index}
+              className={clsx(
+                "w-7 h-7 rounded-full text-white flex items-center justify-center text-sm -mr-1",
+                BGS[index % BGS.length]
+              )}
+            >
+              <UserInfo user={m} />
             </div>
-          </td>
-    
-          <td className='py-2'>
-            <div className='flex gap-1 items-center'>
-              <span className={clsx("text-lg", PRIOTITYSTYLES[task.priority])}>
-                {ICONS[task.priority]}
-              </span>
-              <span className='capitalize'>{task.priority}</span>
-            </div>
-          </td>
-    
-          <td className='py-2'>
-            <div className='flex'>
-              {task.team.map((m, index) => (
-                <div
-                  key={index}
-                  className={clsx(
-                    "w-7 h-7 rounded-full text-white flex items-center justify-center text-sm -mr-1",
-                    BGS[index % BGS.length]
-                  )}
-                >
-                  <UserInfo user={m} />
-                </div>
-              ))}
-            </div>
-          </td>
-          <td className='py-2 hidden md:block'>
-            <span className='text-base text-gray-600'>
-              {moment(task?.date).fromNow()}
-            </span>
-          </td>
-        </tr>
-      );
-      return (
-        <>
-          <div className='w-full md:w-2/3 bg-white px-2 md:px-4 pt-4 pb-4 shadow-md rounded'>
-            <table className='w-full'>
-              <TableHeader />
-              <tbody>
-                {tasks?.map((task, id) => (
-                  <TableRow key={id} task={task} />
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </>
-      );
-    };
-    
+          ))}
+        </div>
+      </td>
+      <td className='py-2 hidden md:block'>
+        <span className='text-base text-gray-600'>
+          {moment(task?.date).fromNow()}
+        </span>
+      </td>
+    </tr>
+  );
+
+  return (
+    <div className='w-full bg-white px-2 md:px-4 pt-4 pb-4 shadow-md rounded'>
+      <table className='w-full'>
+        <TableHeader />
+        <tbody>
+          {tasks?.map((task, id) => (
+            <TableRow key={id} task={task} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 const WallsDashboard = () => {
-
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const totals = summary.tasks;
 
   const stats = [
@@ -137,64 +132,65 @@ const WallsDashboard = () => {
   ];
 
   const Card = ({ label, count, bg, icon }) => {
-      return (
-        <div className='w-full h-32 backdrop-filter backdrop-blur-lg  md:p-8 sm:p-10 ss:p-30 cta-block border-solid border-2 border-indigo-600  p-5 shadow-md rounded-md flex items-center justify-between'>
-          <div className='h-full flex flex-1 flex-col justify-between'>
-            <p className={` ${styles.paragraph} text-base font-semibold`}>{label}</p>
-            <span className='text-2xl font-regular text-white font-raleway'>{count}</span>
-            <span className='text-sm text-gray-400'>{"110 last month"}</span>
-          </div>
-  
-          <div
-            className={clsx(
-              "w-10 h-10 rounded-full flex items-center justify-center text-white",
-              bg
-            )}
-          >
-            {icon}
-          </div>
+    return (
+      <div className='w-full h-32 backdrop-filter backdrop-blur-lg md:p-8 sm:p-10 ss:p-30 cta-block border-solid border-2 border-indigo-600 p-5 shadow-md rounded-md flex items-center justify-between'>
+        <div className='h-full flex flex-1 flex-col justify-between'>
+          <p className={` ${styles.paragraph} text-base font-semibold`}>{label}</p>
+          <span className='text-2xl font-regular text-white font-raleway'>{count}</span>
+          <span className='text-sm text-gray-400'>{"110 last month"}</span>
         </div>
-      );
-    };
+        <div
+          className={clsx(
+            "w-10 h-10 rounded-full flex items-center justify-center text-white",
+            bg
+          )}
+        >
+          {icon}
+        </div>
+      </div>
+    );
+  };
 
-return (
-  <section>
-  <div className='w-full h-screen flex flex-col md:flex-row'>
-       <div className='w-1/5 h-screen bg-indigo-700 sticky top-0 hidden md:block'>
-         <Sidebar />
-       </div>
-       {/* <MobileSidebar /> */}
+  return (
+    <section className='flex flex-col min-h-screen'>
+      <div className='w-full flex flex-col md:flex-row flex-1'>
+        <div className='w-1/5 bg-indigo-700 sticky top-0 hidden md:block'>
+          <Sidebar />
+        </div>
+     {/* Mobile Sidebar */}
+     <MobileSidebar
+          isSidebarOpen={isSidebarOpen}
+          closeSidebar={() => setIsSidebarOpen(false)}
+        />
 
- {/* Grid */}
-         
- <div className='h-full py-4 w-full pl-2 pr-6'>
-          <div className='grid grid-cols-1 md:grid-cols-4 gap-5'>
+        {/* Main Content */}
+        <div className='flex-1 flex flex-col py-4 px-2 md:px-6'>
+          <header className='w-full flex justify-between items-center p-4 bg-white shadow-md md:hidden'>
+            <button onClick={() => setIsSidebarOpen(true)} className='text-2xl'>
+              <MdMenu />
+            </button>
+          </header>
+        <div className='flex-1 flex flex-col py-4 px-2 md:px-6'>
+          <div className='grid grid-cols-1 md:grid-cols-4 gap-5 mb-4'>
             {stats.map(({ icon, bg, label, total }, index) => (
               <Card key={index} icon={icon} bg={bg} label={label} count={total} />
             ))}
           </div>
-          <div className='p-2 2xl:px-6'>
-    <Outlet />
-    </div>
-    <div className={`${styles.paddingX} bg-indigo-700 w-full z-`}>
-    <div className='w-full flex flex-col md:flex-row gap-3 2xl:gap-5 py-6'>
-      {/* /left */}
-
-      <TaskTable tasks={summary.last10Task} />
-    </div>
-    </div>
-    
-          
-    </div>
-    
-    </div>
-    <BackToTopButton />
-<div className={`${styles.paddingX} bg-indigo-700 w-full overflow-hidden`}>
-          <Footer/>
+          <div className='flex-1'>
+            <Outlet />
+          </div>
+          <div className='bg-indigo-700 p-6'>
+            <TaskTable tasks={summary.last10Task} />
+          </div>
+        </div>
       </div>
-    
-  </section>
+      </div>
+      <BackToTopButton />
+      <div className={`${styles.paddingX} bg-indigo-700 w-full overflow-hidden`}>
+        <Footer />
+        </div>
+    </section>
   );
 };
 
-export default WallsDashboard
+export default WallsDashboard;
