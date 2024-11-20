36fbae1741940562129490e37b29211e5db6ebfc
Webamm <engr.saminasim@gmail.com>
2024-11-20 20:05:37 +0500

    fixed conflicts

--- src/utils/AdminRoute.jsx
+++ src/utils/AdminRoute.jsx
@@ -1,26 +1,22 @@
 import { Navigate, useLocation } from 'react-router-dom';
-<<<<<<< Updated upstream
-import AuthService from '../services/AuthService'; 
-=======
+
 import AuthService from '../services/AuthService';
->>>>>>> Stashed changes
+
+
 
 const AdminRoute = ({ children, ...rest }) => {
+  const location = useLocation();
   const isAuth = AuthService.isAuthenticated();
   const user = AuthService.getUser()?.role;
-<<<<<<< Updated upstream
-=======
-  const location = useLocation();
->>>>>>> Stashed changes
+
+
 
   if (user !== 'admin') {
     return <Navigate to="/" replace />
   }
 
-<<<<<<< Updated upstream
-  const location = useLocation();
-=======
->>>>>>> Stashed changes
+
+
 
   return isAuth ? (
     children
